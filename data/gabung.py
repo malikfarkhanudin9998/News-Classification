8ddf056101ccab505156eda9f68b7913d78a24fe
Malik Farkhanudin <100341777+malikfarkhanudin9998@users.noreply.github.com>
2022-03-20 12:08:36 +0700

    Delete data directory

--- data/gabung.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-t1 = pd.read_csv('uji.csv')
-t2 = pd.read_csv('tu2.csv')
-
-# menyatukan file csv dengan concat
-frames = [t1,t2]
-result = pd.concat(frames).drop_duplicates().reset_index(drop=True)
-result.to_csv(r'dataujiv2.csv',index=False)
